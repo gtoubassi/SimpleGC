fa5e151e301f44b511d6ed586afbf146f3b09c08
Garrick Toubassi <gtoubassi@gmail.com>
2015-12-27 17:52:12 -0800

    Initial Commit

---
+++ SimpleGC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimpleGC
+//
+//  Created by Garrick Toubassi on 12/27/15.
+//  Copyright (c) 2015 simple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
